2afb1adb2780e9290eee2759e5f4e0e05350ca1c
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-02-03 17:09:51 +0900

    chore: 초기 세팅

---
+++ .storybook/main.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    // 추가
+    "@storybook/preset-scss",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+};
