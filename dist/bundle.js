e4c3638f11c34413e3f195a95b7b84c8387f8ba9
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-02-12 15:59:10 +0900

    chore: DTM-#3 번들러 세팅

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'react-dom';
+
+var Hello = function Hello() {
+  return /*#__PURE__*/React.createElement("div", null, "Hello change");
+};
+
+export { Hello };
+//# sourceMappingURL=bundle.js.map
