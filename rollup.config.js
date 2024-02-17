932a5c641127c09ded9f88e816b9a5f1dff20a65
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-02-17 20:57:06 +0900

    chore: DTM-#3 빌드 환경 세팅

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,44 @@
 import babel from "@rollup/plugin-babel";
 import typescript from "@rollup/plugin-typescript";
-
-export default {
-  input: "./src/index.ts",
-  output: {
-    file: "./dist/bundle.js",
-    format: "es",
-    sourcemap: true,
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { dts } from "rollup-plugin-dts";
+export default [
+  {
+    input: "./src/index.tsx",
+    output: [
+      {
+        file: "dist/index.cjs",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.mjs",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      babel({
+        babelHelpers: "bundled",
+        presets: [
+          "@babel/preset-env",
+          "@babel/preset-react",
+          "@babel/preset-typescript",
+        ],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      }),
+      typescript(),
+      resolve(),
+      commonjs(),
+      peerDepsExternal(),
+    ],
+    external: ["react", "crypto-js"],
   },
-  plugins: [
-    babel({
-      babelHelpers: "bundled",
-      presets: [
-        "@babel/preset-env",
-        "@babel/preset-react",
-        "@babel/preset-typescript",
-      ],
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
-    }),
-    typescript(),
-  ],
-};
+  {
+    input: "./src/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "cjs" }],
+    plugins: [dts()],
+  },
+];
