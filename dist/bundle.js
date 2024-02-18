dd3f7c3a083ac935c0cc0e422db22125439b19bd
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-02-18 14:01:09 +0900

    chore: DTM-#5 .gitignore 중 이미 올라간 파일 삭제

--- dist/bundle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'react-dom';
-
-var Hello = function Hello() {
-  return /*#__PURE__*/React.createElement("div", null, "Hello change");
-};
-
-export { Hello };
-//# sourceMappingURL=bundle.js.map
