8fed6f2b6e30622f66a802bba0041d958ea5e04f
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-05-21 22:47:07 +0900

    feat: pkce 코어 함수화

--- src/core/pkce.ts
+++ src/core/pkce.ts
@@ -1,10 +1,13 @@
 import CryptoJS from "crypto-js";
 
-const codeVerifier = CryptoJS.lib.WordArray.random(32).toString(
-  CryptoJS.enc.Base64
-);
-const codeChallenge = CryptoJS.SHA256(codeVerifier).toString(
-  CryptoJS.enc.Base64
-);
-
-export { codeVerifier, codeChallenge };
+const getCodeVerifier = () => {
+  return CryptoJS.lib.WordArray.random(32).toString(
+    CryptoJS.enc.Base64url
+  ) as string;
+};
+const getCodeChallenge = (codeVerifier: string) => {
+  return CryptoJS.SHA256(codeVerifier).toString(
+    CryptoJS.enc.Base64url
+  ) as string;
+};
+export { getCodeVerifier, getCodeChallenge };
