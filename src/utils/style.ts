75955dd82e527fd0bb24b04f59a4682756e04486
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-05-21 22:46:11 +0900

    feat: 버튼 스타일링

---
+++ src/utils/style.ts
@@ -0,0 +1,15 @@
+import { CSSProperties } from "react";
+export const DEFAULT_STYLE: CSSProperties = {
+  display: "inline-block",
+  padding: "0px",
+  width: "250px",
+  height: "50px",
+  lineHeight: "40px",
+  color: "white",
+  backgroundColor: "black",
+  border: "1px solid transparent",
+  borderRadius: "8px",
+  fontSize: "16px",
+  fontWeight: "bold",
+  textAlign: "center",
+};
